75aa4d785adc62e86cb5d3b93962c93d47f88844
cho3ek <cho3ek@gmail.com>
2013-12-11 01:40:31 +0100

    choose seat done

--- script/chooseSeats.js
+++ script/chooseSeats.js
@@ -1,19 +1,24 @@
+
+
+
 $(document).ready(function () {
 
     var settings = {
-        rows: 5,
-        cols: 15,
+        rows: 9,
+        cols: 20,
         rowCssPrefix: 'row-',
         colCssPrefix: 'col-',
-        seatWidth: 35,
-        seatHeight: 35,
+        seatWidth: 45,
+        seatHeight: 45,
         seatCss: 'seat',
         selectedSeatCss: 'selectedSeat',
         selectingSeatCss: 'selectingSeat'
     };
-
+	
+		var price = 14.99;
     var init = function (reservedSeat) {
         var count = 1;
+		var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","W","X","Y","Z"];
         var str = [], seatNo, className;
         for (i = 0; i < settings.rows; i++) {
             for (j = 0; j < settings.cols; j++) {
@@ -24,7 +29,7 @@ $(document).ready(function () {
                 }
                 str.push('<li class="' + className + '"' +
                   'style="top:' + (i * settings.seatHeight).toString() + 'px;left:' + (j * settings.seatWidth).toString() + 'px">' +
-                  '<a title="' + seatNo + '">' + seatNo + '</a>' +
+                  '<a style="font-size:9px;color:#888;font-weight:bold;background-color:#dadada;" title="<tr style=vertical-align:middle><td style=width:45px;><img src=images/seats/green.png style=float:left;padding:5px;padding-top:0px;width:35px; /></td><td style=width:55px;>Row: <b>' + letters[i] + '</b></td><td style=width:55px;>Seat: <b>' + j +'</b></td><td><small>&ensp; €' + price +'</small></td></tr>">' +'</a>' +
                   '</li>');
                 count = count +1;
             }
@@ -36,33 +41,37 @@ $(document).ready(function () {
             //init();
 
             //Case II: If already booked
-            var bookedSeats = [50,51,52,53,54,55];
+            var bookedSeats = [54,55,56,82,83];
+			var i = 108;
+			while(i<200){
+				i++;
+				bookedSeats.push(i);
+			}
             init(bookedSeats);
 
 
             $('.' + settings.seatCss).click(function () {
              if ($(this).hasClass(settings.selectedSeatCss)){
-                alert('This seat is already reserved');
+                alert('Sorry! This seat is already reserved');
             }
             else{
                 $(this).toggleClass(settings.selectingSeatCss);
             }
         });
 
-        //     $('#btnShow').click(function () {
-        //         var str = [];
-        //         $.each($('#place li.' + settings.selectedSeatCss + ' a, #place li.'+ settings.selectingSeatCss + ' a'), function (index, value) {
-        //             str.push($(this).attr('title'));
-        //         });
-        //         alert(str.join(','));
-        //     })
+				 $("#tickets").text('');
+                 $("#tickets").append('<span style="text-transform:uppercase;font-size:14px;text-decoration:underline;font-weight:bold;">Selected seats:</span>&nbsp;&nbsp; 0<br/>'); 
+                
 
-        //     $('#btnShowNew').click(function () {
-        //         var str = [], item;
-        //         $.each($('#place li.' + settings.selectingSeatCss + ' a'), function (index, value) {
-        //             item = $(this).attr('title');                   
-        //             str.push(item);                   
-        //         });
-        //         alert(str.join(','));
-        //     })
+             $('#place li').click(function () {
+                 var str = [], item;
+                 $.each($('#place li.' + settings.selectingSeatCss + ' a'), function (index, value) {
+                     item = $(this).attr('title');                   
+                     str.push(item);                   
+                 });
+				
+                 $("#tickets").text(''); 
+                 $("#tickets").append('<span style="text-transform:uppercase;font-size:14px;text-decoration:underline;font-weight:bold;">Selected seats:</span>&nbsp;&nbsp;' + str.length +'&nbsp;&nbsp;<br/>&nbsp;<br/><table>' + str.join('') + '</table><div style="margin-left:170px;border-top:1px solid #dadada; padding:5px;"><b>€ '+price*(str.length)+'</b></div>'); 
+				 //alert(str.join(','));
+             })
         });
